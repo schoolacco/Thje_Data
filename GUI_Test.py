8a75a9a29c60a9555da97d2cedcfee2d62677dae
schoolacco <anthony.revel510@education.nsw.gov.au>
2024-07-30 11:09:55 +1000

    Added a GUI test

---
+++ GUI_Test.py
@@ -0,0 +1,8 @@
+from tkinter import *
+root = Tk()
+root.title("Epik GUI")
+root.configure(background= "black")
+root.maxsize(1000,1000)
+root.minsize(100,100)
+root.geometry("500x500+20+100")
+root.mainloop()
