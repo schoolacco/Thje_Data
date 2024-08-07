7d409c762050dd4e850a06e89b2cd5cd79f9d403
schoolacco <anthony.revel510@education.nsw.gov.au>
2024-08-07 11:10:04 +1000

    Finished the research and made a data dictionary, messed around with text in a GUI :)

--- GUI_Test.py
+++ GUI_Test.py
@@ -5,4 +5,11 @@ root.configure(background= "black")
 root.maxsize(1000,1000)
 root.minsize(100,100)
 root.geometry("500x500+20+100")
+
+
+#Label Test
+text = Label(root, text="It's a GUI :D")
+text.pack()
+text2 = Label(root, text="Yipee!")
+text2.pack()
 root.mainloop()
